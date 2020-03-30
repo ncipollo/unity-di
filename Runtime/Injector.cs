683f4d39c67e52dfae2da6636fee085ccb8e4ef0
Nick Cipollo <njc1115@gmail.com>
2020-03-29 21:40:56 -0400

    Implement Injector

--- Runtime/Injector.cs
+++ Runtime/Injector.cs
@@ -1,5 +1,22 @@
-﻿namespace UnityDI {
-    public class Injector {
+﻿using System;
 
+namespace UnityDI {
+    public class Injector {
+        private readonly Dependencies dependencies;
+
+        public static Injector Shared = new Injector();
+
+        public Injector() {
+            dependencies = new Dependencies();
+        }
+
+        public void Module(Action<Module> moduleFactory) {
+            var module = new Module(dependencies);
+            moduleFactory(module);
+        }
+
+        public T Get<T>(string name = "") => dependencies.Get<T>(name);
+
+        public T Inject<T>(string name = "") => Get<T>(name);
     }
 }
