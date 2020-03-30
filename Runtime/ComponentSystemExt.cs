7e8fa9eb25d5c7281824b0fc37a4017242350464
Nick Cipollo <njc1115@gmail.com>
2020-03-29 22:51:38 -0400

    Add ComponentSystemExt

---
+++ Runtime/ComponentSystemExt.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace UnityDI {
+    public static class ComponentSystemExt {
+        public static void Get<T>(this ComponentSystemBase system, string name = "") =>
+            Injector.Shared.Get<T>(name);
+
+        public static void Inject<T>(this ComponentSystemBase system, string name = "") =>
+            Injector.Shared.Inject<T>(name);
+    }
+}
